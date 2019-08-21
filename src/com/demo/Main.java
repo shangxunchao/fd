3e2015d5e3bd7da3e3d73e347372885ecd2c91a5
Administrator <838375225@qq.com>
2019-08-22 00:04:01 +0800

    dgf

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,9 +2,9 @@ package com.demo;
 
 public class Main {
     public static void main(String[] args) {
-        int a;
+        int a=0;
         System.out.println("dsdsdssfsdf");
         System.out.println("dsdsdssfsdf");
-        System.out.println("sdf");
+        System.out.println("a="+a);
     }
 }
