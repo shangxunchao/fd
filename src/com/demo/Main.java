de5fd6a5fd0e644c1f5c6fc5a865fa29fb091266
shangxunchao <838375225@qq.com>
2019-08-21 23:51:01 +0800

    Update Main.java

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -2,9 +2,9 @@ package com.demo;
 
 public class Main {
     public static void main(String[] args) {
-
+        int a;
         System.out.println("dsdsdssfsdf");
         System.out.println("dsdsdssfsdf");
-        System.out.println("dsdsdssfsdf");
+        System.out.println("sdf");
     }
 }
